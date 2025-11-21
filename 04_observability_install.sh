0f701245343595a28b113f1a097d42c5ca2ef382
damien <damien.clementdhuart@gmail.com>
2025-11-21 23:16:48 +0100

    wip grafana

--- 04_observability_install.sh
+++ 04_observability_install.sh
@@ -3,6 +3,6 @@ kubectl create namespace meta
 
 helm repo add grafana https://grafana.github.io/helm-charts
 
-helm install meta-monitoring grafana/meta-monitoring --version 12.3.0 -n meta -f grafana-values.yaml
+helm install meta-monitoring grafana/meta-monitoring --version 1.3.0 -n meta -f grafana-values.yaml
 
 kubectl get pods -n meta
